15d28c332a3f968fb094c77d2af6a771b8e48e0b
Mohan89en <mohangowdu630@gmail.com>
2023-04-19 23:33:26 +0530

    .

--- src/struct_ex.rs
+++ src/struct_ex.rs
@@ -22,5 +22,8 @@ pub fn run(){
      let sweet = Design{flower: Flowers::jasmine,
           patterns_no: 6.0};
      print_design(sweet);
+      
+     let new_design = Design{flower:Flowers::lily,patterns_no: 5.0};
+     print_design(new_design)
+     }
 
-}
